5791113b074c1396e8d252951420345d3f4c6251
fahimhossainmunna <hossainfahim306@gmail.com>
2025-10-03 23:43:13 +0600

    first commit

---
+++ firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnqQqX-NtTN4WecILXdHrr3h7vhRcmiuE",
+  authDomain: "fdr2051crud.firebaseapp.com",
+  projectId: "fdr2051crud",
+  storageBucket: "fdr2051crud.firebasestorage.app",
+  messagingSenderId: "772486618681",
+  appId: "1:772486618681:web:0c92509f717d45d36e77e8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
